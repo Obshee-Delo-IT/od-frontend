75df94f52a664c8fdcdbc74dfcb6ab71189d8f2b
fatfirebird <fatfirebird@gmail.com>
2025-07-07 18:29:31 +0400

    add some rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +11,89 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), eslintConfigPrettier];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintConfigPrettier,
+  {
+    rules: {
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/prop-types': 'off',
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.tsx', '.jsx'],
+        },
+      ],
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react/hook-use-state': [
+        'error',
+        {
+          allowDestructuredState: false,
+        },
+      ],
+      'react/iframe-missing-sandbox': 'warn',
+      'react/jsx-boolean-value': 'error',
+      'react/jsx-child-element-spacing': 'warn',
+      'react/jsx-curly-brace-presence': [
+        'warn',
+        {
+          props: 'never',
+          children: 'never',
+          propElementValues: 'always',
+        },
+      ],
+      'react/jsx-fragments': ['warn', 'syntax'],
+      'react/self-closing-comp': ['warn'],
+      '@typescript-eslint/no-namespace': 'off',
+      'import/no-default-export': 'error',
+      'import/prefer-default-export': 'off',
+      'import/no-cycle': 'error',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [['external', 'builtin'], 'internal', ['parent', 'sibling'], 'index', 'object', 'type'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'no-plusplus': 'error',
+      curly: ['error', 'all'],
+      camelcase: 'error',
+      'no-console': 'warn',
+      'arrow-body-style': ['error', 'as-needed'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+];
 
 export default eslintConfig;
