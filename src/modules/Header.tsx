722a2514ed45d49a851cc4c518ee8ade52f54cea
Polina <polina.osintseva000@gmail.com>
2025-08-06 17:01:30 +0400

    added header for mobile

--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -1,19 +1,88 @@
-import { Button } from '@radix-ui/themes';
+import { Button, IconButton } from '@radix-ui/themes';
+import clsx from 'clsx';
 import Logo from '@/ui/assets/icons/logo.svg';
-import SearchIcon from '@/ui/assets/icons/search.svg';
+import { ButtonGroup } from '@/ui/components/ButtonGroup';
+import { MenuIcon, SearchIcon } from '@/ui/components/Icons';
 import { Input } from '@/ui/components/input';
 import css from './Header.module.css';
 
 export const Header = () => (
-  <div className={css.wrapper}>
-    <div className={css.container}>
-      <Logo className={css.logo} />
-      <div className={css.input}>
-        <Input color="red" placeholder="Поиск по сайту" rightIcon={<SearchIcon />} />
+  <div className={css.header}>
+    <div className={clsx(css.container, css.headerDesktop)}>
+      <div className={css.top}>
+        <div className={css.logoCont}>
+          <Logo className={css.logo} />
+        </div>
+        <div className={css.searchBar}>
+          <div className={css.input}>
+            <Input color="red" placeholder="Поиск по сайту" rightIcon={<SearchIcon />} />
+          </div>
+          <Button size="4" variant="outline">
+            Оказать помощь
+          </Button>
+        </div>
       </div>
-      <Button size="4" color="gray" variant="soft">
-        Оказать помощь
-      </Button>
+      <div className={css.bottom}>
+        <ButtonGroup
+          items={[
+            { href: '/test', id: 1, text: 'ГЛАВНАЯ' },
+            {
+              href: '/test',
+              id: 2,
+              text: 'О НАС',
+              content: [
+                {
+                  href: '/test',
+                  id: 1,
+                  text: 'Общее дело',
+                },
+                {
+                  href: '/test',
+                  id: 2,
+                  text: 'Команда',
+                },
+                {
+                  href: '/test',
+                  id: 3,
+                  text: 'Удостоверение',
+                },
+                {
+                  href: '/test',
+                  id: 4,
+                  text: 'Благодарственные письма',
+                },
+                {
+                  href: '/test',
+                  id: 5,
+                  text: 'Истории активистов',
+                },
+                {
+                  href: '/test',
+                  id: 6,
+                  text: 'Экспертные заключения и документы',
+                },
+              ],
+            },
+            { href: '/test', id: 3, text: 'ПРОГРАММЫ' },
+            { href: '/test', id: 4, text: 'ВИДЕО' },
+            { href: '/test', id: 6, text: 'ПРИМИ УЧАСТИЕ' },
+            { href: '/test', id: 7, text: 'МАТЕРИАЛЫ' },
+            { href: '/test', id: 8, text: 'ОБЩЕЕ ДЕЛО-ПРО' },
+            { href: '/test', id: 9, text: 'КОНТАКТЫ' },
+          ]}
+        />
+      </div>
+    </div>
+    <div className={clsx(css.container, css.headerMobile)}>
+      <div className={css.logoCont}>
+        <Logo className={css.logo} />
+      </div>
+      <IconButton variant="solid" radius="medium">
+        <SearchIcon />
+      </IconButton>
+      <IconButton variant="outline" radius="medium">
+        <MenuIcon />
+      </IconButton>
     </div>
   </div>
 );
