9c83d9b07d815101dab8b424e02fdd2c7094e84d
fatfirebird <fatfirebird@gmail.com>
2025-11-08 16:16:14 +0400

    migrate to next 16

--- next.config.ts
+++ next.config.ts
@@ -2,32 +2,42 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  webpack: (config) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'));
+  // webpack: (config) => {
+  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //   const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'));
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
+  //   config.module.rules.push(
+  //     {
+  //       ...fileLoaderRule,
+  //       test: /\.svg$/i,
+  //       resourceQuery: /url/,
+  //     },
+
+  //     {
+  //       test: /\.svg$/i,
+  //       issuer: fileLoaderRule.issuer,
+  //       resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+  //       use: ['@svgr/webpack'],
+  //     }
+  //   );
+
+  //   fileLoaderRule.exclude = /\.svg$/i;
+
+  //   return config;
+  // },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
-
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
+    },
   },
+
   images: {
     remotePatterns: [new URL('/**', process.env.WP_BASE), new URL('/**', 'https://xn----9sbkcac6brh7h.xn--p1ai')],
   },
+  reactCompiler: true,
 };
 
 export default nextConfig;
